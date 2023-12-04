b8ce9507687c60c6083e559cff32de0e32b29668
mEng120506 <engibaryan.maria@gmail.com>
2023-12-04 18:04:40 +0300

    commit5/2

--- PZ_5/5.1.py
+++ PZ_5/5.1.py
@@ -1,18 +1,15 @@
 #Составить программу, в которой функция генерирует четырехзначное число и определяет, есть ли в числе одинаковые цифры.
-import random # импортируем модуль random для генерации случайных чисел
 
-def generate_number():
-    return random.randint(1000, 9999) # генерируем четырехзначное случайное число
+import random
 
-def check_duplicates(number): # объявляем функцию check_duplicates с аргументом number
-    digits = [int(a) for a in str(number)] # преобразуем число в список цифр
-    return len(digits) != len(set(digits)) # возвращаем True, если есть одинаковые цифры, иначе False
+def check_duplicates(number):
+    digits = [int(a) for a in str(number)]
+    return len(digits) != len(set(digits)) #set - множество
 
-number = generate_number()  # вызываем функцию generate_number и сохраняем результат в переменной number
+number = random.randint(1000, 9999)
 print("Сгенерированное число:", number)
 
-if check_duplicates(number): # проверяем наличие одинаковых цифр в числе с помощью функции check_duplicates
+if check_duplicates(number):
     print("В числе есть одинаковые цифры.")
 else:
     print("В числе нет одинаковых цифр.")
-
