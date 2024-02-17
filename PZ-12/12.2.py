86d831ba8fb6ac2eb3ea2e04df22c4a75945e6ee
mEng120506 <engibaryan.maria@gmail.com>
2024-02-17 10:10:43 +0300

    commit12.3

--- PZ-12/12.2.py
+++ PZ-12/12.2.py
@@ -2,8 +2,8 @@
 #you can specify third-party standalone applications and run them as External Tools'.
 
 import  string
-str = 'In PyCharm, you can specify third-party standalone applications and run them as External Tools'
-a = [i for i in str if i in string.ascii_lowercase]
+stroka = 'In PyCharm, you can specify third-party standalone applications and run them as External Tools'
+a = [i for i in stroka if i in string.ascii_lowercase]
 print(a)
 
 
